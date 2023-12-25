69d445ecf2be2e0dfff2c20e5030daf8cbd39ef7
zant123456 <davidcth123456@gmail.com>
2023-12-25 12:47:18 +0900

    Update player.cs

--- Assets/player.cs
+++ Assets/player.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public CharacterController _controller;
+    public Rigidbody _rigidbody;
     public Collider _collider;
     public float _speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
         _collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
     {
         //get input from the user
         //use wasd for movement
@@ -27,7 +32,15 @@ public class Player : MonoBehaviour
         //move the player based on the input
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
         Vector3 velocity = direction * _speed;
-        _controller.Move(velocity * Time.deltaTime);
+        Console.WriteLine(velocity);
+        _rigidbody.velocity = velocity;
+        //_rigidbody.AddForce(velocity * Time.deltaTime, ForceMode.Impulse);
+
+        //stop if no input
+        if(horizontalInput == 0 && verticalInput == 0)
+        {
+            _rigidbody.velocity = Vector3.zero;
+        }
 
     }
 
@@ -41,6 +54,6 @@ public class Player : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        _speed = 5.0f; 
+        _speed = 5.0f;
     }
 }
