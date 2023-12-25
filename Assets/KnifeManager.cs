35031c10563a1d9e331611b3997f0680c5ae9409
Fukuda-Kenta <fukuda.kenta0607@gmail.com>
2023-12-25 14:10:04 +0900

    aaa

--- Assets/KnifeManager.cs
+++ Assets/KnifeManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class KnifeManager : MonoBehaviour
 {
+    private bool isKnufeEnd = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +15,21 @@ public class KnifeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
+        Invoke("UpdateKnife", 3.0f);　// 関数を3秒後に実行
+
     }
 
     private void UpdateKnife()
     {
-        transform.position += new Vector3(-5, 0, 0) * Time.deltaTime;//ナイフの移動
-      //  if()
+        if (isKnufeEnd == false)
+        {
+            transform.position += new Vector3(-20, 0, 0) * Time.deltaTime;//ナイフの移動
+
+            if (transform.position.x < -30.0f)
+            {
+                isKnufeEnd = true;
+            }
+        }
     }
 }
